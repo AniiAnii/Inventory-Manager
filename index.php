2c16e5b7622b89330fa9b4856da53d1fb52ae64d
AniiAnii <151471174+AniiAnii@users.noreply.github.com>
2024-02-22 16:24:31 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Unos novog dela</title>
+    <style>
+        /* Stilizacija linka kao dugmeta */
+        .dugme {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #ccc;
+            /* Siva boja */
+            color: #fff;
+            /* Bijela boja teksta */
+            text-decoration: none;
+            /* Uklanja podvučen link */
+            border-radius: 5px;
+            /* Zaobljeni rubovi */
+            transition: background-color 0.3s;
+            /* Glatka promjena boje */
+        }
+
+        .dugme:hover {
+            background-color: #999;
+            /* Siva boja na hover */
+        }
+    </style>
+</head>
+
+<body>
+
+    <div style="text-align: right;">
+        <a href="unos_novog_dela.php" class="dugme">Unos novog dela</a>
+    </div>
+
+    <h2>Unos novog dela</h2>
+
+</body>
+
+</html>
