ed659664473e82171a114713192e7df09b1842f9
AniiAnii <151471174+AniiAnii@users.noreply.github.com>
2024-02-29 23:27:37 +0100

    Update praksaa.sql

--- praksa.sql
+++ praksa.sql
@@ -1,31 +1,27 @@
+drop database praksaa;
 create database praksaa;
 use praksaa;
-
-
 -- Kreiranje tablice Materijali
 CREATE TABLE Materijali (
     MaterijalID INT PRIMARY KEY AUTO_INCREMENT,
     Naziv VARCHAR(255),
-    Kolicina DECIMAL(10,2),
-    Cijena DECIMAL(10,2)
+    Kolicina DECIMAL(10, 2),
+    Cijena DECIMAL(10, 2)
 );
-
 -- Kreiranje tablice Proizvodi
 CREATE TABLE Proizvodi (
     ProizvodID INT PRIMARY KEY AUTO_INCREMENT,
     Naziv VARCHAR(255),
     MaterijalID INT,
-    KolicinaNaSkladistu DECIMAL(10,2),
+    KolicinaNaSkladistu DECIMAL(10, 2),
     FOREIGN KEY (MaterijalID) REFERENCES Materijali(MaterijalID)
 );
-
--- Kreiranje tablice Porudzbine
-CREATE TABLE Porudzbine (
-    PorudzbinaID INT PRIMARY KEY AUTO_INCREMENT,
-    DatumPorudzbine DATE,
-    DatumIsporuke DATE
-);
-
+/*-- Kreiranje tablice Porudzbine
+ CREATE TABLE Porudzbine (
+ PorudzbinaID INT PRIMARY KEY AUTO_INCREMENT,
+ DatumPorudzbine DATE,
+ DatumIsporuke DATE
+ );*/
 -- Tablica koja povezuje Proizvode i Porudzbine
 CREATE TABLE PorudzbinaStavke (
     PorudzbinaID INT,
@@ -33,4 +29,22 @@ CREATE TABLE PorudzbinaStavke (
     Kolicina INT,
     FOREIGN KEY (PorudzbinaID) REFERENCES Porudzbine(PorudzbinaID),
     FOREIGN KEY (ProizvodID) REFERENCES Proizvodi(ProizvodID)
+);
+-- Kreiranje tablice Delovi
+CREATE TABLE Delovi (
+    RedniBroj INT PRIMARY KEY AUTO_INCREMENT,
+    Sifra VARCHAR(255),
+    Naziv VARCHAR(255),
+    VrstaMaterijala VARCHAR(255),
+    Zastita VARCHAR(255),
+    KomadiIzSipke INT,
+    MeraProizvodaGrami DECIMAL(10, 2)
+);
+CREATE TABLE Porudzbine (
+    PorudzbinaID INT PRIMARY KEY AUTO_INCREMENT,
+    DatumZaTermicku DATE,
+    VrstaTermickeObrade VARCHAR(255),
+    DatumIsporukeDelova DATE,
+    Kolicina INT,
+    GrPotrebnihSipki DECIMAL(10, 2)
 );
